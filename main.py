41d71727230ece4e204846e7f044e151eca45de4
Katerina <e.mayakovskaya@gmail.com>
2025-09-11 17:13:33 +0300

    Publish repo

---
+++ main.py
@@ -0,0 +1,88 @@
+import asyncio
+from pprint import pprint
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from environs import env
+import httpx
+
+env.read_env()
+dp = Dispatcher()
+TOKEN = env.str('TG_TOKEN')
+ALLOWED_CHAT_ID = env.int('ALLOWED_CHAT_ID')
+
+
+async def get_api_response(bot, chat_id):
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {
+        "Authorization": f"Token {env.str('API_KEY')}"
+    }
+    params = {"timestamp": None}
+
+    async with httpx.AsyncClient() as client:
+        while True:
+            try:
+                response = await client.get(url, headers=headers, params=params, timeout=90)
+                data = response.json()
+
+                if data.get("status") == "found":
+                    pprint(data)
+                    await send_notification(bot, chat_id, data)
+                    params["timestamp"] = data.get("last_attempt_timestamp")
+
+                elif data.get("status") == "timeout":
+                    print("Новых данных нет")
+                    params["timestamp"] = data.get("timestamp_to_request")
+
+            except httpx.ConnectError:
+                print("Проблемы с соединением. Повторная попытка через 5 секунд...")
+                await asyncio.sleep(5)
+            except httpx.ReadTimeout:
+                print("Сервер не отвечает. Повторный запрос через 5 секунд...")
+                await asyncio.sleep(5)
+
+
+async def send_notification(bot, chat_id, data):
+    new_attempts = data.get("new_attempts")
+    if not new_attempts:
+        await bot.send_message(chat_id, text='Обнаружена проверка, но деталей нет :(')
+        return
+    attempt = new_attempts[0]
+
+    text = (
+        f"Твою работу проверили!\n"
+        f"Получено код-ревью по уроку: {attempt.get('lesson_title')}\n"
+        f"Статус работы: {'нуждается в доработке' if attempt.get('is_negative') else 'принята'}\n"
+        f"Можно посмотреть по ссылке: {attempt.get('lesson_url')}"
+    )
+    await bot.send_message(chat_id, text=text)
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    chat_id = message.chat.id
+    if chat_id != ALLOWED_CHAT_ID:
+        await message.answer(f"Прости, но я проверяю только Катины домашние работы!")
+    else:
+        await message.answer(f"Привет, Катя! Начинаю следить за твоими работами!")
+
+
+@dp.message()
+async def text_message_handler(message: Message):
+    chat_id = message.chat.id
+    if chat_id != ALLOWED_CHAT_ID:
+        await message.answer(f"Прости, но я проверяю только Катины домашние работы!")
+    else:
+        text = 'Я слишком туп, чтобы что-то отвечать, но я жив и все еще слежу за твоими работами!'
+        await message.answer(text=text)
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    asyncio.create_task(get_api_response(bot, ALLOWED_CHAT_ID))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
